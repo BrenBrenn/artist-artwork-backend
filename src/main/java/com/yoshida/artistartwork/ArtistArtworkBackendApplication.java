f11ba9992feb9e90c3c06c3e3a631134342341c5
BrenBren <anrbrl@gmail.com>
2025-06-30 20:41:38 -0400

    initial commit

---
+++ src/main/java/com/yoshida/artistartwork/ArtistArtworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yoshida.artistartwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistArtworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistArtworkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoshida/artistartwork/ArtistArtworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshida.artistartwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistArtworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
